bb5fb2b4cac57099b657ec403e2969f0c3f224aa
wd1ror1 <mzlxch@gmail.com>
2019-03-22 03:47:18 -0400

    Before finish and has bug

---
+++ app/javascript/components/Search.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import SearchResultList from '.SearchResultList';
+
+export default class Search extends React.Component {
+    state = { loading: false, results: []};
+    onChange = (e) => {
+        const { value } = e.target;
+        this.setState({ loading: true });
+        if (value.length > 0) {
+            axios.get('/search.json', { params: {query: value } })
+                .then(res => this.setState({ loading: false, results: res.data }))
+                .catch(() => this.setState({ loading:false, results: []}));              
+        } else {
+            this.setState({ loading: false, results: []});
+        }
+    }
+    render() {
+        const { loading, results } = this.state;
+        return (
+        <div className="ui raised segment no padding">
+            <form method="GET" action="search">
+            <div className="ui fluid icon transparent large input">
+                <input name="query" type="text" placeholder="Search apps..." onChange={this.onChange} autoComplete="off" />
+                <button type="submit">
+                <i className="search icon"></i>
+                </button>
+            </div>
+            {results.length > 0 || loading ? <SearchResultList results={results} loading={loading} /> : null }
+            </form>
+        </div>
+        );
+    }
+}
